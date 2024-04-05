11497dde2623b8ff000e6535c9276585149becd2
santiDev <xantirendonc@gmail.com>
2024-04-05 12:42:34 -0500

    💄 update logo theme class and refactoring css

--- src/lib/components/site/navbar.svelte
+++ src/lib/components/site/navbar.svelte
@@ -3,8 +3,31 @@
 	import { routes } from '$lib/config';
 	import { cn } from '$lib/utils';
 	import { ThemeToggle } from '$lib/components/site';
+	import {theme} from "$lib/stores"
+	import { onMount } from 'svelte';
 
+	let themeClass:string;
 	let openToWork = true;
+
+	onMount(()=>{
+		if (localStorage.getItem('mode')?.replace(/^"(.*)"$/, '$1') === 'light') {
+			themeClass = 'light-mode--filter'
+		} else {
+			themeClass = 'dark-mode--filter'
+		}
+	})
+
+	theme.subscribe((value)=>{
+		console.log('suscribe',value)
+
+		if (value === 'light') {
+			themeClass = 'light-mode--filter'
+		} else {
+			themeClass = 'dark-mode--filter'
+		}
+	})
+
+	
 </script>
 
 <nav
@@ -21,7 +44,7 @@
 					class="text-5xl text-transparent bg-gradient-to-r from-primary to-gray-400 bg-clip-text md:text-6xl"
 					>P</span
 				>rabhu -->
-				<img src="../../../../static/logo/png/logo-no-background.png" class="theme-mode--filter" alt="logo">
+				<img src="../../../../static/logo/png/logo-no-background.png" class="{themeClass} w-24" alt="logo">
 			</a>
 			{#if openToWork}
 				<div class="flex items-center border border-border rounded-xl px-2 gap-3">
@@ -62,10 +85,10 @@
 </nav>
 
 <style>
-	.theme-mode--filter{
-		width: 100px;
-		height: auto;
-
-		/* filter: invert(99%) sepia(1%) saturate(974%) hue-rotate(246deg) brightness(116%) contrast(100%); */
+	.dark-mode--filter{
+		filter: invert(99%) sepia(1%) saturate(974%) hue-rotate(246deg) brightness(116%) contrast(100%);
+	}
+	.light-mode--filter{
+		filter: invert(0%) sepia(0%) saturate(1%) hue-rotate(135deg) brightness(101%) contrast(101%);
 	}
 </style>
