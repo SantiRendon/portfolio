0fef2c4dbb513da9f3fb65951a229a59657f7fa2
santiDev <xantirendonc@gmail.com>
2024-04-10 20:04:46 -0500

    🔧 add translation in hero component

--- src/lib/components/site/hero.svelte
+++ src/lib/components/site/hero.svelte
@@ -3,6 +3,7 @@
 	import { SocialButtons } from '$lib/components/site';
 	import { whatsNew } from '$lib/config';
 	import { Mailbox } from 'lucide-svelte';
+	import { _ } from 'svelte-i18n';
 
 	// const ProfilePicture = new URL('../../assets/profile_picture.jpg', import.meta.url).href
 </script>
@@ -30,13 +31,10 @@
 				</div>
 			</div> -->
 
-			<h1 class="font-bold md:text-xl">Hi, I'm</h1>
+			<h1 class="font-bold md:text-xl">{$_('home_greeting')}</h1>
 			<h1 class="text-2xl font-bold md:text-5xl">Santiago Rendón Castrillón</h1>
 			<p>
-				a software developer from India. I specialize in Python, Machine Learning, and full stack
-				development. I’m currently seeking software developer roles to further my career. In my free
-				time, I enjoy writing technical blogs and sharing my journey with others. If you'd like to
-				get in touch, feel free to reach out to me.
+				{$_('home_presentation')}
 			</p>
 			<SocialButtons />
 		</div>
